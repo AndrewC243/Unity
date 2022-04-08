ac670a98d986d37ead0c5a9307cb5039a0a9064f
AndrewC243 <60679323+AndrewC243@users.noreply.github.com>
2022-04-08 09:46:52 -0500

    Comments

--- PlayerSprite.cs
+++ PlayerSprite.cs
@@ -41,15 +41,15 @@ public class PlayerSprite : MonoBehaviour
         }
     }
 
-    public bool getFriendlyTurn() {
+    public bool getFriendlyTurn() { // Naming was because I was originally going to make a full team in this class, but wasn't sure if that was the greatest idea
         return myTurn;
     }
     
-    public void changeTurn() {
+    public void changeTurn() { // I don't have the time in class to make a full system for clicking and selecting characters to move, so instead they'll move 1 at a time with WASD
         myTurn = !myTurn;
     }
 
-    public static GameObject inst(float x, float y, float z) {
+    public static GameObject inst(float x, float y, float z) { // I don't even know man, I'm just trying whatever I can to make this game work
         return Instantiate(Test, new Vector3(x, y, z), Quaternion.identity);
     }
 }
